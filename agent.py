51099268b4bc6ddc8439c9c085f9c27024dcd5ac
Sheldon Hsin-Peng Lin <wenli1973@gmail.com>
2024-06-25 15:35:47 +0800

    First commit.

---
+++ agent.py
@@ -0,0 +1,51 @@
+import sys, json
+# Import relevant functionality
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.prebuilt import create_react_agent
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_openai import AzureChatOpenAI
+
+###### Azure OpenAI Settings #######
+with open('param.json', 'r', encoding='utf-8') as param_file:
+    param_data = json.load(param_file)
+    azure_apikey = param_data["azure_apikey"]
+    azure_apibase  = param_data["azure_apibase"]
+    azure_apitype = param_data["azure_apitype"]
+    azure_apiversion = param_data["azure_apiversion"]
+    azure_gptx_deployment = param_data["azure_gptx_deployment"]
+    azure_embd_deployment = param_data["azure_embd_deployment"]
+param_file.close()
+####################################
+
+def main():
+    try:
+        print("Hello Agent!")
+
+        # Create the agent
+        memory = SqliteSaver.from_conn_string(":memory:")
+        model = AzureChatOpenAI(deployment_name=azure_gptx_deployment, openai_api_version=azure_apiversion, openai_api_key=azure_apikey, azure_endpoint=azure_apibase, temperature=0.9)
+        wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+        tools = [wikipedia]
+        agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+        # Use the agent
+        config = {"configurable": {"thread_id": "abc123"}}
+        for chunk in agent_executor.stream(
+            {"messages": [HumanMessage(content="hi im bob! and i live in taoyuan city")]}, config
+        ):
+            print(chunk)
+            print("----")
+
+        for chunk in agent_executor.stream(
+            {"messages": [HumanMessage(content="who is the president of the country of where I live?")]}, config
+        ):
+            print(chunk)
+            print("----")
+        
+    except ValueError as ve:
+        return str(ve)
+
+if __name__ == "__main__":
+    sys.exit(main())
