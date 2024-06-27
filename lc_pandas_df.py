7b93c651353811e27047739674ca96d56fa63b8e
Sheldon Hsin-Peng Lin <wenli1973@gmail.com>
2024-06-27 17:04:17 +0800

    backlog

--- lc_pandas_df.py
+++ lc_pandas_df.py
@@ -1,5 +1,8 @@
 import sys, json
+import pandas as pd
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
 ###### Azure OpenAI Settings #######
 with open('param.json', 'r', encoding='utf-8') as param_file:
@@ -15,7 +18,7 @@ param_file.close()
 
 def main():
     try:
-        print("Hello LangChain!")
+        print("Hello LangChain Pandas Dataframe!")
 
         model = AzureChatOpenAI(deployment_name=azure_gptx_deployment, openai_api_version=azure_apiversion, openai_api_key=azure_apikey, azure_endpoint=azure_apibase, temperature=0)
         embed = AzureOpenAIEmbeddings(deployment=azure_embd_deployment, openai_api_key=azure_apikey, openai_api_version=azure_apiversion, openai_api_type=azure_apitype, azure_endpoint=azure_apibase)
@@ -23,7 +26,21 @@ def main():
         run_option = 0        
         match run_option:
             case 0:
-                print('Hello')
+                df = pd.read_csv("./data/titanic.csv")
+
+                # The ZERO_SHOT_REACT_DESCRIPTION agent
+                agent = create_pandas_dataframe_agent(model, df, verbose=True, allow_dangerous_code=True)
+
+                # The OPENAI_FUNCTIONS agent
+                agent1 = create_pandas_dataframe_agent(
+                    model,
+                    df,
+                    verbose=True,
+                    agent_type=AgentType.OPENAI_FUNCTIONS,
+                    allow_dangerous_code=True,
+                )
+                
+                print(agent.invoke("how many rows are there?"))
             case _:
                 print(f'Error: Wrong run_option({run_option})!')
 
